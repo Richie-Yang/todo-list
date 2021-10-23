a319a60d552f4774ff2d9c520287874f9b9b215f
Richie-Yang <richieyang8813@gmail.com>
2021-10-23 13:30:44 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 const port = 3000
+
+
+mongoose.connect('mongodb://localhost/todo-list')
+// get db connection status
+const db = mongoose.connection
+
+// connection error
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+// connection successful
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 
 app.get('/', (req, res) => {
---
+++ models/todo.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+
+const todoSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  }
+})
+
+
+module.exports = mongoose.model('Todo', todoSchema)
