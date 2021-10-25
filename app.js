c83724308c8f5991539c4c2cf437ee8b4ce1b019
Richie-Yang <richieyang8813@gmail.com>
2021-10-25 10:01:27 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const Todo = require('./models/todo')
 const app = express()
 
 const port = 3000
@@ -29,7 +30,10 @@ app.set('view engine', 'hbs')
 
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find()
+      .lean()
+      .then(todos => res.render('index', { todos }))
+      .catch(error => console.error(error))
 })
 
 app.listen(port, () => {
