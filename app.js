8f438d093abb76335e28aaedd0202d68fe9a6c67
Richie-Yang <richieyang8813@gmail.com>
2021-10-28 22:28:14 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const Todo = require('./models/todo')
 const { urlencoded } = require('body-parser')
 const app = express()
@@ -31,6 +32,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 // read all items in CRUD
 app.get('/', (req, res) => {
@@ -70,7 +72,7 @@ app.get('/todos/:id/edit', (req, res) => {
 })
 
 // update a specific item in CRUD
-app.post('/todos/:id/edit', (req, res) => {
+app.put('/todos/:id', (req, res) => {
   const id = req.params.id
   const { name, isDone } = req.body
   return Todo.findById(id)
@@ -84,7 +86,7 @@ app.post('/todos/:id/edit', (req, res) => {
 })
 
 // delete a specific item in CRUD
-app.post('/todos/:id/delete', (req, res) => {
+app.delete('/todos/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .then(todo => todo.remove())
