9b1772e0e0ca570acdc831575f794e8596597573
Richie-Yang <richieyang8813@gmail.com>
2021-11-12 12:19:51 +0800

    feat: add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -19,11 +19,27 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符!' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors, name, email, password, confirmPassword
+    })
+  }
+
   return User.findOne({ email })
     .then(user => {
       if (user) {
         console.log('User already exists')
-        res.render('register', req.body)
+        return res.render('register', {
+          errors, name, email, password, confirmPassword
+        })
       } else {
         return User.create({ name, email, password })
           .then(() => res.redirect('/'))
@@ -34,6 +50,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
